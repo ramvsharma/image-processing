8ebcf072d5ccb5aeb8c6152093bdde67a24d4ee3
Ram Vijay <ramvijaysharma55@gmail.com>
2020-08-26 12:23:21 +0530

    2_2 is corrected

--- 1(a)/2_2.py
+++ 1(a)/2_2.py
@@ -3,9 +3,26 @@ import numpy as np
 
 img = cv2.imread("cameraman.png")
 
-newImage = np.ones((256,256,3))
+changed = np.zeros((256,256,3))
 
-x = img[:128,:128,:]
+C11 = img[:128,:128,:]
+# cv2.imshow("C11",C11)
 
-cv2.imshow("Ram",x)
+C12 = img[128:,:128,:]
+# cv2.imshow("C12",C12)
+
+C21 = img[:128,128:,:]
+# cv2.imshow("C21",C21)
+
+C22 = img[128:,128:,:]
+# cv2.imshow("C22",C22)
+
+
+changed[:128,:128,:] = C22/255;
+changed[128:,:128,:] = C21/255;
+changed[:128,128:,:] = C12/255;
+changed[128:,128:,:] = C11/255;
+
+
+cv2.imshow("Combined",changed)
 cv2.waitKey(0)
